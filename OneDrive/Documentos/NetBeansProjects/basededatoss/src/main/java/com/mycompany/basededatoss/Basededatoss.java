c80b4625a5fa13ab5afbec4b3b95ffd757bb332a
Andres24-MQ <andresochoa011@gmail.com>
2025-02-03 10:26:29 -0700

    sql work

---
+++ OneDrive/Documentos/NetBeansProjects/basededatoss/src/main/java/com/mycompany/basededatoss/Basededatoss.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basededatoss;
+
+/**
+ *
+ * @author andre
+ */
+public class Basededatoss {
+
+    public static void main(String[] args) {
+        InicioSesion inicio =  new InicioSesion();
+        
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+    }
+}
