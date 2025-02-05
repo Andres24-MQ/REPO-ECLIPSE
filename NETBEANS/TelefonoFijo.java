4108c5f6ff1c28c8268a6315811d5a7e6a44ef4d
Andres24-MQ <andresochoa011@gmail.com>
2025-02-04 22:52:17 -0700

    cambios

---
+++ NETBEANS/TelefonoFijo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquetee;
+public class TelefonoFijo extends Telefono {
+	private int Extension;
+	
+public TelefonoFijo(String marca, String modelo, int numero,int extension) {
+		super(marca, modelo,numero);
+		
+	}
+public TelefonoFijo(){}
+public int getExtension() {
+	return Extension;
+}
+public void setExtension(int extension) {
+	Extension = extension;
+}
+
+
+
+}
